61acc3392c03f1d003fc95720f76fc92799cad0a
Can Batuhan <icanbatuhan@gmail.com>
2023-05-05 15:54:27 +0300

    KendineMarkt
     Basit Online Alışveriş Uygulaması - Basic Online Shopping App

---
+++ src/pages/Detail/Detail.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Text, Image, View } from "react-native";
+import useFetch from "../../hooks/useFetch/useFetch";
+import Error from "../../components/Error/Error";
+import Loading from "../../components/Loading/Loading";
+import styles from "./Detail.style";
+import {API_URL} from "@env";
+
+const Detail = ({route}) => {
+    const {id} = route.params;
+    const {loading, error, data} = useFetch(API_URL + "/" + id);
+    console.log(id);
+
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (error) {
+        return <Error />;
+    }
+
+    return(
+        <View style={styles.container}>
+            <Image source={{uri: data.image}} style={styles.image} />
+            <View style={styles.body_container}>
+            <Text style={styles.title}>{data.title}</Text>
+            <Text style={styles.desc}>{data.description}</Text>
+            <Text style={styles.price}>{data.price} TL </Text>
+            </View>
+        </View>
+    );
+};
+
+export default Detail;
---
+++ src/pages/Products/Products.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text,SafeAreaView, FlatList, ActivityIndicator} from "react-native";
+import {API_URL} from "@env";
+import axios from "axios";
+
+import ProductCard from "../../components/ProductCard/";
+import useFetch from "../../hooks/useFetch/useFetch";
+import Loading from "../../components/Loading/Loading";
+import Error from "../../components/Error/Error";
+
+const Products = ({navigation}) => {
+
+    const {loading,data,error} = useFetch(API_URL);
+
+    const handleProductSelect = id => {
+        navigation.navigate("DetailPage" , {id});
+    };
+
+    const renderProduct = ({item}) => ( 
+    <ProductCard product={item} onSelect={() => handleProductSelect(item.id)}/>
+    );
+
+    if(loading){
+        return <Loading />;
+    }
+
+    if(error) {
+        return <Error />;
+    }
+
+    return(   
+            <FlatList data={data} renderItem={renderProduct} />
+    );
+};
+
+export default Products;
